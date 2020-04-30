11d7ad074913e48a9e8bd24085fd36e7eb835e9e
CodyBrewer <codybrewerdev@gmail.com>
2020-04-30 13:08:47 -0600

    added production configuration to knex

--- knexfile.js
+++ knexfile.js
@@ -25,4 +25,17 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
